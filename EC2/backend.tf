93fc355913b6abb32e91968f711f21cc6f753fe5
sandhya2317 <sandhya234.j@gmail.com>
2025-09-02 13:06:49 +0530

    Update backend.tf

--- EC2/backend.tf
+++ EC2/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
 
-    bucket  = "statefilestorage8780"
-    key     = "mystatefolder2/terraform.tfstate"
+    bucket  = "githubactionss3bucket"
+    key     = "myfolder/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
   }
