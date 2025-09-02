2debdb4971196ff331bb8510ffb5c3575d2222ca
sandhya2317 <sandhya234.j@gmail.com>
2025-09-02 15:32:08 +0530

    Update resource.tf

--- EC2/resource.tf
+++ EC2/resource.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myinstance" {
   ami           =  data.aws_ami.ubuntu.id
   instance_type = var.myinstype
-  key_name = var.key-name              
+  key_name = var.key_name              
   tags = {
     Name = "My EC2 Instance"
   }
